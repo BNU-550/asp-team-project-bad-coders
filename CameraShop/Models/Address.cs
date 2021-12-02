1f0b5639054a42461780a352d68a5582acbe54b2
kianroz122 <kianroz122@gmail.com>
2021-12-02 14:27:16 +0000

    Started adding models

---
+++ CameraShop/Models/Address.cs
@@ -0,0 +1,10 @@
+﻿namespace CameraShop.Models
+{
+    public class Address
+    {
+        public int AddressId { get; set; }
+        public string HouseNumber { get; set; }
+        public string Postcode { get; set; }
+
+    }
+}
---
+++ CameraShop/Models/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CameraShop.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public int AddressID { get; set; }
+
+        [Required, StringLength(20), Display(Name ="First Name")]
+        public string FirstName { get; set; }
+        
+        public string LastName { get; set; }
+        public string Email { get; set; }  
+        public string PhoneNumber { get; set; }
+        //Navigation Property
+        public virtual Address Address { get; set; }
+
+    }
+}
