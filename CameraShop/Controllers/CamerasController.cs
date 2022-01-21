246f19231b7c39b200fe8ae4b26783168e6df788
kianroz122 <kianroz122@gmail.com>
2022-01-21 12:32:24 +0000

    Crud for cameras

---
+++ CameraShop/Controllers/CamerasController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CameraShop.Data;
+using CameraShop.Models;
+
+namespace CameraShop.Controllers
+{
+    public class CamerasController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CamerasController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Cameras
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Cameras.ToListAsync());
+        }
+
+        // GET: Cameras/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var camera = await _context.Cameras
+                .FirstOrDefaultAsync(m => m.CameraId == id);
+            if (camera == null)
+            {
+                return NotFound();
+            }
+
+            return View(camera);
+        }
+
+        // GET: Cameras/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Cameras/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CameraId,Make,Model,CameraType,Summary,Description,Image,VideoUrl,CurrentPrice,CarouselNumber,Discount")] Camera camera)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(camera);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(camera);
+        }
+
+        // GET: Cameras/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var camera = await _context.Cameras.FindAsync(id);
+            if (camera == null)
+            {
+                return NotFound();
+            }
+            return View(camera);
+        }
+
+        // POST: Cameras/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("CameraId,Make,Model,CameraType,Summary,Description,Image,VideoUrl,CurrentPrice,CarouselNumber,Discount")] Camera camera)
+        {
+            if (id != camera.CameraId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(camera);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CameraExists(camera.CameraId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(camera);
+        }
+
+        // GET: Cameras/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var camera = await _context.Cameras
+                .FirstOrDefaultAsync(m => m.CameraId == id);
+            if (camera == null)
+            {
+                return NotFound();
+            }
+
+            return View(camera);
+        }
+
+        // POST: Cameras/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var camera = await _context.Cameras.FindAsync(id);
+            _context.Cameras.Remove(camera);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CameraExists(int id)
+        {
+            return _context.Cameras.Any(e => e.CameraId == id);
+        }
+    }
+}
